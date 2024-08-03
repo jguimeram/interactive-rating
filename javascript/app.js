8335cc45a05d39737ef8d7ce2b28fe9f00d839df
jguimeram <jguimera54@gmail.com>
2024-08-03 11:58:43 +0200

    it works as expected

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,35 @@
 console.log('hello, world')
 
-const root = document.getElementById('root')
-const btn = document.querySelector('.submit-btn')
+const ratioNumber = document.getElementById('ratio-number')
+const submit = document.querySelectorAll('.submit-btn')
 const numbers = document.querySelectorAll('.btn')
+const container = document.querySelector('.container');
+const containerReverse = document.querySelector('.container-reverse');
+let ratio = null;
 
 numbers.forEach(number => {
     number.addEventListener('click', (e) => {
-        let ratio = e.target.dataset.id
-        root.textContent = ratio
+        ratio = e.target.dataset.id
     })
 })
+
+submit.forEach(e => {
+    e.addEventListener('click', () => {
+        e.preventDefault;
+        ratioNumber.textContent = ratio
+        if (ratio) {
+            if (!container.classList.contains('hidden')) {
+                container.classList.add('hidden')
+                containerReverse.classList.remove('hidden')
+            } else {
+                container.classList.remove('hidden')
+                containerReverse.classList.add('hidden')
+                ratio = null;
+            }
+        } else {
+            window.alert('select a number, madafaka')
+        }
+
+    })
+})
+
