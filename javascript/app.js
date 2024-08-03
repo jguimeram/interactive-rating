40ffc78d4bb16cccfb5e5f7edf0e0c093a66b7fe
jguimeram <jguimera54@gmail.com>
2024-08-03 07:58:40 +0200

    js

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,12 @@
 console.log('hello, world')
+
+const root = document.getElementById('root')
+const btn = document.querySelector('.submit-btn')
+const numbers = document.querySelectorAll('.btn')
+
+numbers.forEach(number => {
+    number.addEventListener('click', (e) => {
+        let ratio = e.target.dataset.id
+        root.textContent = ratio
+    })
+})
